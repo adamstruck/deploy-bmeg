3eacd99ad788aa941e95c621f628f4357bc1817b
Adam Struck <adamjstruck@gmail.com>
2019-09-27 23:30:26 +0000

    wip

--- etl/load_database.sh
+++ etl/load_database.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "$#" -ne 2 ]; then
 		printf "Illegal number of parameters.\n\n"
 		printf "Usage:\n	load_database.sh <graph> <file_manifest>\n"
@@ -7,8 +9,13 @@ if [ "$#" -ne 2 ]; then
 fi
 
 graph=$1
-file_manifest=$2
+file_manifest=$(realpath $2)
+release_dir=$(dirname $file_manifest)
 
+echo "linking $release_dir to ./outputs"
+ln -s $release_dir outputs
+
+echo "checking that all files in the manifest exist"
 for f in $(cat $file_manifest); do
 		if [ ! -f $f ]; then
 				echo "file $f does not exist!"
@@ -16,10 +23,19 @@ for f in $(cat $file_manifest); do
 		fi
 done
 
-# grip drop --host test-grip-mongo:18202 $graph
-# grip create --host test-grip-mongo:18202 $graph
+while true; do
+    read -p "Do you wish to drop the graph before proceeding with the database load? " yn
+    case $yn in
+        [Yy]* ) echo "dropping graph: $graph"; grip drop $graph --host grip:8202; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
-gofast="--numInsertionWorkers 24 --writeConcern 0 --bypassDocumentValidation --host=test-mongo"
+# ensure graph exists
+grip create $graph --host grip:8202
+
+gofast="--numInsertionWorkers 24 --writeConcern 0 --bypassDocumentValidation --host=mongo"
 
 for f in $(cat $file_manifest | grep "Vertex"); do
 		if [[ $f =~ \.gz$ ]]; then
@@ -36,3 +52,6 @@ for f in $(cat $file_manifest | grep "Edge"); do
 				mongoimport -d grip -c ${graph}_edges --type json --file $f $gofast
 		fi
 done
+
+echo "removing soft link ./outputs"
+rm outputs
--- grip-kv/load_kv.sh
+++ grip-kv/load_kv.sh
@@ -3,6 +3,6 @@
 cd /data
 cat bmeg_file_manifest.txt | grep Vertex > vertex_manifest.txt
 cat bmeg_file_manifest.txt | grep Edge > edge_manifest.txt
-{ time grip kvload bmeg_rc2 --db ./db --edge-manifest edge_manifest.txt --vertex-manifest vertex_manifest.txt ; } 2> timings/kvload_time.txt
+{ time grip kvload bmeg_rc3 --db ./db --edge-manifest edge_manifest.txt --vertex-manifest vertex_manifest.txt ; } 2> timings/kvload.stderr
 grip server --config /config/grip_config.yml
 tail -f /dev/null
